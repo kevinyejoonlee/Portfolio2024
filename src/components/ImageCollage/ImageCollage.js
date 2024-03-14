df94690100741f4652d04d125bbaa85a7800338f
Kevin Yejoon Lee <Catalpa.maple@gmail.com>
2024-03-14 13:03:16 -0400

    reverted wait till load

--- src/components/ImageCollage/ImageCollage.js
+++ src/components/ImageCollage/ImageCollage.js
@@ -1,53 +1,41 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import VanillaTilt from 'vanilla-tilt';
 
 import './ImageCollage.css';
 
-import guitar from "../../assets/CollagePhotos/guitar2.jpg";
-import kcphoto from "../../assets/CollagePhotos/kcphoto3.jpg";
-import laptop from "../../assets/CollagePhotos/laptop.jpg";
-import BJJ from "../../assets/CollagePhotos/bjj_win.JPG";
-import team from "../../assets/CollagePhotos/team_photo.JPG";
+import guitar from "../../assets/CollagePhotos/guitar2.jpg"; 
+import kcphoto from "../../assets/CollagePhotos/kcphoto3.jpg"; 
+import laptop from "../../assets/CollagePhotos/laptop.jpg"; 
+import BJJ from "../../assets/CollagePhotos/bjj_win.JPG"; 
+import team from "../../assets/CollagePhotos/team_photo.JPG"; 
 
 const ImageCollage = () => {
   const tiltRef = useRef(null);
-  const [imagesLoaded, setImagesLoaded] = useState(0);
-
-  const images = [guitar, kcphoto, laptop, BJJ, team];
-  const totalImages = images.length; 
 
   useEffect(() => {
-    if (imagesLoaded === totalImages) {
-      const nodes = tiltRef.current.querySelectorAll('.imageFormat');
-      VanillaTilt.init(nodes, {
-        max: 25,
-        speed: 400,
-      });
+    const nodes = tiltRef.current.querySelectorAll('.imageFormat');
+    VanillaTilt.init(nodes, {
+      max: 25,
+      speed: 400,
 
-      nodes.forEach((node, index) => {
-        const tiltDegree = [0, 2, 3].includes(index) ? 2 : -2;
-        node.style.transform = `rotate(${tiltDegree}deg)`;
-      });
-    }
-  }, [imagesLoaded, totalImages]); 
-
-  const handleImageLoad = () => {
-    setImagesLoaded(prev => prev + 1); 
-  };
+    });
+  
+    nodes.forEach((node, index) => {
+      const tiltDegree = [0, 2, 3].includes(index) ? 2 : -2; 
+      node.style.transform = `rotate(${tiltDegree}deg)`;
+    });
+    
+  }, []);
+  
 
   return (
     <section className='ImageCollage' ref={tiltRef}>
       <div className="Collage_Container">
-        {images.map((imageSrc, index) => (
-          <img
-            key={index}
-            className="imageFormat"
-            src={imageSrc}
-            alt={`image-${index}`}
-            onLoad={handleImageLoad} 
-            style={{ visibility: imagesLoaded === totalImages ? 'visible' : 'hidden' }} 
-          />
-        ))}
+        <img className="imageFormat" src={guitar} alt="guitar"></img>
+        <img className="imageFormat" src={kcphoto} alt="kcphoto"></img>
+        <img className="imageFormat" src={laptop} alt="laptop"></img>
+        <img className="imageFormat" src={BJJ} alt="BJJ"></img>
+        <img className="imageFormat" src={team} alt="team"></img>
       </div>
     </section>
   );
