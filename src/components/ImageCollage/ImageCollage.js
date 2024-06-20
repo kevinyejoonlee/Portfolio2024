925089be6256546dc5455c6f7688b3bcbd08deae
Kevin Yejoon Lee <Catalpa.maple@gmail.com>
2024-06-20 16:32:50 -0400

    small cahnges

--- src/components/ImageCollage/ImageCollage.js
+++ src/components/ImageCollage/ImageCollage.js
@@ -4,37 +4,23 @@ import VanillaTilt from 'vanilla-tilt';
 import './ImageCollage.css';
 
 import guitar from "../../assets/CollagePhotos/guitar2.jpg"; 
-import kcphoto from "../../assets/CollagePhotos/kcphoto3.jpg"; 
+// import kcphoto from "../../assets/CollagePhotos/kcphoto3.jpg"; 
+import tunnel from "../../assets/CollagePhotos/tunnel.JPG"; 
 import laptop from "../../assets/CollagePhotos/laptop.jpg"; 
-import BJJ from "../../assets/CollagePhotos/bjj_win.JPG"; 
+import traveled from "../../assets/CollagePhotos/saltlake.JPG"; 
 import team from "../../assets/CollagePhotos/team_photo.JPG"; 
 
 const ImageCollage = () => {
-  const tiltRef = useRef(null);
 
-  useEffect(() => {
-    const nodes = tiltRef.current.querySelectorAll('.imageFormat');
-    VanillaTilt.init(nodes, {
-      max: 25,
-      speed: 400,
-
-    });
-  
-    nodes.forEach((node, index) => {
-      const tiltDegree = [0, 2, 3].includes(index) ? 2 : -2; 
-      node.style.transform = `rotate(${tiltDegree}deg)`;
-    });
-    
-  }, []);
   
 
   return (
-    <section className='ImageCollage' ref={tiltRef}>
+    <section className='ImageCollage'>
       <div className="Collage_Container">
         <img className="imageFormat" src={guitar} alt="guitar"></img>
-        <img className="imageFormat" src={kcphoto} alt="kcphoto"></img>
-        <img className="imageFormat" src={laptop} alt="laptop"></img>
-        <img className="imageFormat" src={BJJ} alt="BJJ"></img>
+        <img className="imageFormat" src={laptop} alt="kcphoto"></img>
+        <img className="imageFormat" src={tunnel} alt="laptop"></img>
+        <img className="imageFormat" src={traveled} alt="BJJ"></img>
         <img className="imageFormat" src={team} alt="team"></img>
       </div>
     </section>
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -2,64 +2,55 @@ import React, { useState } from 'react';
 import './Work.css';
 
 
-import workpng from "../../assets/symbols/Work.png";
-import downloadCSV from "../../assets/symbols/downloadCSV.png";
+// import workpng from "../../assets/symbols/Work.png";
+// import downloadCSV from "../../assets/symbols/downloadCSV.png";
 
 
 const jobs = {
 
 
-  kc: {
-    Company: 'Kingdom Come',
-    Title: 'Software Developer ',
-    date: "Jan 2024 – Present",
-    details: [
-        "•	Assisting a lead developer in devising solutions to automate club operations and streamlining routine administrative tasks.",
-       "• Engineered a novel application that converts music lyrics into dynamic slideshows, enhancing the visual experience of song presentations and performances.",
-       "• Implemented an interactive feedback system within the club’s app, allowing for real-time member engagement and continuous improvement of club activities."
-      ]
-  
-  },
-
   ctrl: {
     Company: 'CTRL + Y',
     Title: "Web Developer",
     date: "October 2023 – Jan 2024",
-   
-    details: ["• Built a dedicated sponsor blurb section with interactive elements using Vue.js for the home landing page.",
-        "• Collaborated with teams to quickly resolve development issues and enhance continuous improvement of project outcomes",
-        "• Employed media CSS techniques for responsive design, achieving high consistency across various browsers and devices",
-        "• Assigned to lead the design and implementation of user-centric features to enhance the mobile user interface on the website"]
+    details: [
+        "• Developed and launched a responsive website using Vue.Js by incorporating design elements and accessibility features.",
+        "• Led the design and implementation of user-centric features to enhance the mobile user interface, resulting in improved usability and engagement.",
+        "• Collaborated with teams to resolve development issues and drive continuous improvement of project outcomes.",
+        "• Created a dedicated sponsor blurb section with dynamic elements for an engaging home landing experience."
+      ]
   
   },
 
   WLU: {
     Company: 'Wilfrid Laurier University',
-    Title: 'Teaching & Instructional Lab Assistant, Exam Proctor ',
+    Title: 'Programming Instructor',
     date: "May 2023 – October 2023",
     details: [
-        "• Managed the grading process of assignments, personalized feedback for 75+ students within a 3-day turnaround.",
-        "• Focused on providing teaching assistance for courses that matched personal academic interests, like Python, Java, Digital Electronics, & Assembly programming.",
-        "• Evaluated and orchestrated weekly laboratory sessions for a diverse group of 30+ students, providing insightful feedback and fostering active engagement.",
-        "• Selected by the Administrator for exceptional academic performance, lead to a return invitation for another term."
-      ],
-
-  Chatime: {
-    Company: 'Kinc',
-    Title: 'Softwae Developer ',
-    date: "Jan 202– Present",
-    details: [
-        "•	Axvising solutions to automate club operations and streamlining routine administrative tasks.",
-        
+        "• Facilitated student’s learning one-on-one and in group settings, teaching Data Structures and Algorithms, Python, Java, Digital Electronics, and Assembly programming concepts, improving student’s understanding and practical skills.",
+        "• Evaluated and orchestrated weekly laboratory sessions for over 60 students, providing feedback and fostering active engagement, enhancing hands-on learning and student participation.",
+        "• Collaborated with the lead professor to prepare and organize instructional materials, resources, and activities for over 500 students, enhancing the quality and structure of the course content."
       ]
-      
-      },
+    },
+
+    Extra: {
+    Company: 'Other',
+    Title: 'Extracuricular & Certifications',
+    date: "",
+    details: [
+      "• Jarvis Tech Hackathon - 2nd Place",
+      "• Full Stack Web Development Bootcamp Udemy - Dr. Angela Yu",
+      "• Laurier Campus Ambassador - Jarvis Consulting Group",
+      "• Kingdom Come - Praise Leader & Small Groups Leader",
+      "• Sheepgate Fellowship Church - Turkey Missions & Earthquake Relief Work"]
+    
+  
   
   }
 };
 
 function WorkExperience() {
-  const [activeJob, setActiveJob] = useState('kc');
+  const [activeJob, setActiveJob] = useState('ctrl');
   return (
 
     <div className="job_Container">
@@ -88,7 +79,7 @@ const Work = () => {
     <section id='work'>
       <div className="maxwidth-60">
         <h1 className="title">Experience</h1>
-        <p className="paragraph">Here are a few of my work experiences.</p>
+        <p className="paragraph">Here are a few of my experiences.</p>
       </div>
         <div className="workExperience_container">
         
